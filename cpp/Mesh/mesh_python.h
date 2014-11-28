041d613b08b3804a4e12e10805976871efd41b81
Richard Stebbing <richie.stebbing@gmail.com>
2014-11-28 09:34:47 +0000

    Add cpp/Mesh/mesh_python.h

---
+++ cpp/Mesh/mesh_python.h
@@ -0,0 +1,32 @@
+// mesh_python.h
+#ifndef MESH_MESH_PYTHON_H
+#define MESH_MESH_PYTHON_H
+
+// Includes
+#include <algorithm>
+#include <iterator>
+#include <vector>
+#include "Math/linalg_python.h"
+
+// mesh_python
+namespace mesh_python {
+
+using mesh::FaceArray;
+using mesh::GeneralMesh;
+
+// PyArrayObject_to_CellArray
+std::vector<int> PyArrayObject_to_CellArray(
+    PyArrayObject * npy_raw_face_array) {
+  // Construct `raw_face_array` from `npy_raw_face_array`.
+  assert(PyArray_NDIM(npy_raw_face_array) == 1);
+  auto v = linalg_python::PyArrayObject_to_VectorMap<int>(
+    npy_raw_face_array);
+
+  std::vector<int> raw_face_array;
+  std::copy(v->data(), v->data() + v->size(), back_inserter(raw_face_array));
+  return raw_face_array;
+}
+
+} // namespace mesh_python
+
+#endif // MESH_MESH_PYTHON_H
