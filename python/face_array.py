5a92b279ce2a414fe6d830de5b22cf7a48582820
Richard Stebbing <richie.stebbing@gmail.com>
2014-11-28 09:34:53 +0000

    Add python/face_array.py

---
+++ python/face_array.py
@@ -0,0 +1,29 @@
+# face_array.py
+
+# Imports
+import numpy as np
+
+# sequence_to_raw_face_array
+def sequence_to_raw_face_array(T):
+    raw_face_list = []
+    raw_face_list.append(len(T))
+    for i, face in enumerate(T):
+        raw_face_list.append(len(face))
+        raw_face_list += list(face)
+
+    return np.array(raw_face_list, dtype=np.int32)
+
+# raw_face_array_to_sequence
+def raw_face_array_to_sequence(raw_face_array):
+    T = []
+    i = 1
+    while i < len(raw_face_array):
+        n = raw_face_array[i]
+        if i + n + 1 > len(raw_face_array):
+            break
+        T.append(raw_face_array[i + 1: i + n + 1])
+        i += n + 1
+    if len(T) != raw_face_array[0]:
+        raise ValueError('len(T) != raw_face_array[0] (%d vs %d)' %
+                         (len(T), raw_face_array[0]))
+    return T
