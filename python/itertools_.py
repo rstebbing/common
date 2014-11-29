1a57c5952077e07a779d3417b05d42468c0d4c0a
Richard Stebbing <richie.stebbing@gmail.com>
2014-11-29 15:13:58 +0000

    Add python/itertools_.py

---
+++ python/itertools_.py
@@ -0,0 +1,19 @@
+# itertools_.py
+
+# Imports
+from itertools import *
+
+# From (or adapted from) https://doc.python.org/2/library/itertools.html
+
+# flatten
+def flatten(list_of_lists):
+    "Flatten one level of nesting"
+    return chain.from_iterable(list_of_lists)
+
+# pairwise
+def pairwise(s, repeat=False):
+    a, b = tee(s)
+    first = next(b, None)
+    if repeat:
+        b = chain(b, [first])
+    return izip(a, b)
